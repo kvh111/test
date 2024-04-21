8bf5db485d8cba7ffd4c69679e9b30df6136e5c1
K Veethahavya <veetha-havya.kandadai@hpe.com>
2024-04-21 22:26:26 +0530

    test

---
+++ validation_logic.js
@@ -0,0 +1,26 @@
+// const fs = require("fs");
+
+// fs.readFile('/Havya/test/apis/userids.json','utf-8', (err, data) =>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data)
+//     const userIdsObject = JSON.parse(data);
+//     const userIdsArray = userIdsObject['github-userids']
+//     console.log(userIdsArray)
+// })
+
+const authorizedUsers = require('./apis/userids.json');
+
+// Get the pull request author's username from command-line arguments
+const pullRequestAuthor = process.argv[2];
+
+// Check if the pull request author is in the list of authorized users
+if (authorizedUsers.includes(pullRequestAuthor)) {
+  console.log(`${pullRequestAuthor} is an authorized user.`);
+} else {
+  console.log(`${pullRequestAuthor} is not an authorized user. PR rejected.`);
+  process.exit(1); // Exit with a non-zero status code to indicate failure
+}
+
